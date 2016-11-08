dbb1da03d2fce56c6ae47f256899b7afe2c237d9
Jatan Patel <j@jatan.me>
2016-11-07 17:07:19 -0700

    Add Guide-key, Window-numbering, and Evil-surround packages

--- init.el
+++ init.el
@@ -4,8 +4,6 @@
 (setq inhibit-startup-screen t)
 (setq inhibit-startup-message t)
 (defun display-startup-echo-area-message () (message ""))
-(menu-bar-mode -1)
-(tool-bar-mode -1)
 
 (setq configure-frame-functions '())
 
@@ -16,8 +14,11 @@
     (desktop-save-mode 1)
     (setq default-frame-alist desktop-window-attributes)))
 (setq frame-title-format "Emacs - %b")
+(menu-bar-mode -1)
+(tool-bar-mode -1)
 (show-paren-mode 1)
 (blink-cursor-mode 0)
+(setq-default cursor-in-non-selected-windows nil)
 (global-linum-mode 1)
 (setq linum-format " %d ")
 (setq resize-mini-windows t)
@@ -25,6 +26,7 @@
 (setq scroll-error-top-bottom t)
 (setq tooltip-use-echo-area t)
 (add-to-list 'configure-frame-functions '(lambda () (toggle-scroll-bar -1)))
+(global-superword-mode 1)
 (electric-pair-mode 1)
 
 ;; Indentation
@@ -40,8 +42,8 @@
 (set-face-attribute 'default nil :font global-font-face)
 (add-to-list 'configure-frame-functions
   '(lambda ()
-    (set-face-attribute 'vertical-border nil :foreground "#202020")
-    (set-face-attribute 'ido-subdir nil :foreground "#8b7d6b")
+    (set-face-attribute 'vertical-border nil :foreground "#222222")
+    (set-face-attribute 'ido-subdir nil :foreground "#90788c")
     (set-face-attribute 'tabbar-default nil :background "#202328" :foreground "#202328" :box '(:line-width 1 :color "#202328" :style nil) :font global-font-face)
     (set-face-attribute 'tabbar-unselected nil :background "#202328" :foreground "#606060" :box '(:line-width 5 :color "#202328" :style nil))
     (set-face-attribute 'tabbar-selected nil :background "#272b33" :foreground "white" :box '(:line-width 5 :color "#272b33" :style nil))
@@ -59,13 +61,13 @@
 (set-selection-coding-system 'utf-8))
 (prefer-coding-system 'utf-8)
 
+;; Copy/cut entire line if no region is selected
 (defun slick-cut (beg end)
   (interactive
     (if mark-active
       (list (region-beginning) (region-end))
       (list (line-beginning-position) (line-beginning-position 2)))))
 (advice-add 'kill-region :before #'slick-cut)
-
 (defun slick-copy (beg end)
   (interactive
     (if mark-active
@@ -73,6 +75,18 @@
       (message "Copied line")
       (list (line-beginning-position) (line-beginning-position 2)))))
 (advice-add 'kill-ring-save :before #'slick-copy)
+
+; ;; Get rid of buffer messages
+; (defun my-ignore-bet-end-of-buffer--around-ad (f &rest args)
+;   (condition-case nil
+;       (apply f args)
+;     ((beginning-of-buffer end-of-buffer))))
+
+; (advice-add 'left-char :around
+;             #'my-ignore-bet-end-of-buffer--around-ad)
+
+; (advice-add 'right-char :around
+;             #'my-ignore-bet-end-of-buffer--around-ad)
 
 ;; Packages
 (package-initialize)
@@ -88,16 +102,22 @@
     ("7968290e621e86fb44ebfcaa4d17601087ae17b28dd689ddff467179c2983164" "f080d47fc227ba4d7129df8ff5b2aaa9ec50ea242cff220dc3758b3fadd3ef78" "fcaa761fedb6bacfc7b0c0551d3b710568d0da4eb3124bf86f7c6bedf3170296" default)))
  '(package-selected-packages
    (quote
-    (golden-ratio web-mode tide company flycheck js2-mode helm-ag ag helm-projectile general neotree use-package flx-ido tabbar ido-vertical-mode projectile spaceline helm evil))))
+    (window-numbering guide-key evil-surround web-mode tide company flycheck js2-mode helm-ag ag helm-projectile general neotree use-package flx-ido tabbar ido-vertical-mode projectile spaceline helm evil))))
 
-;; Evil
-(setq evil-toggle-key "C-`")
+;; Evil Mode
+(setq evil-toggle-key "C-S-`")
 (require 'evil)
 (evil-mode 1)
 (setq evil-disable-insert-state-bindings t)
 (evil-set-initial-state 'dired-mode 'normal)
 (evil-set-initial-state 'Buffer-menu-mode 'normal)
 (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+(with-eval-after-load 'evil
+  (defalias #'forward-evil-word #'forward-evil-symbol))
+
+;; Evil Surround
+(require 'evil-surround)
+(global-evil-surround-mode 1)
 
 ;; Helm
 (require 'helm)
@@ -108,30 +128,43 @@
 (setq helm-M-x-fuzzy-match t)
 (setq helm-recentf-fuzzy-match t)
 (setq helm-semantic-fuzzy-match t)
+; (add-to-list 'configure-frame-functions '(lambda ()
+;   (set-face-attribute 'helm-match nil :foreground "#5cacee" :underline nil)))
 
 ;; Projectile
 (use-package projectile
-  :diminish projectile-mode
   :init
   (setq projectile-keymap-prefix (kbd "C-l p"))
   :config
-  (projectile-global-mode)
   (setq projectile-use-native-indexing t)
-  (setq projectile-enable-caching t))
+  (setq projectile-enable-caching t)
+  (add-to-list 'projectile-globally-ignored-directories "elpa")
+  (add-to-list 'projectile-globally-ignored-directories ".cache")
+  (add-to-list 'projectile-globally-ignored-directories "node_modules")
+  (projectile-global-mode))
+
+; Window Numbering
+(defun window-numbering-install-mode-line (&optional position))
+(window-numbering-mode 1)
+(dolist (n (number-sequence 1 9))
+  (global-set-key (kbd (format "C-%s" n)) (intern (format "select-window-%s" n))))
 
 ;; Spaceline
 (require 'spaceline-config)
 (spaceline-spacemacs-theme)
 (spaceline-helm-mode 1)
 (setq powerline-default-separator nil)
+(setq spaceline-workspace-numbers-unicode t)
+(setq paceline-window-numbers-unicode t)
 (spaceline-toggle-minor-modes-off)
 (spaceline-toggle-hud-off)
 (spaceline-toggle-buffer-size-off)
 (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
-(set-face-attribute 'spaceline-evil-insert nil :background "#7eaefd")
-(set-face-attribute 'spaceline-evil-normal nil :background "#4f3598" :foreground "#ffffff")
-(set-face-attribute 'spaceline-evil-replace nil :background "#005154" :foreground "#ffffff")
-(set-face-attribute 'spaceline-evil-visual nil :background "#e6987a")
+(add-to-list 'configure-frame-functions '(lambda ()
+  (set-face-attribute 'spaceline-evil-insert nil :background "#7eaefd")
+  (set-face-attribute 'spaceline-evil-normal nil :background "#4f3598" :foreground "#ffffff")
+  (set-face-attribute 'spaceline-evil-replace nil :background "#005154" :foreground "#ffffff")
+  (set-face-attribute 'spaceline-evil-visual nil :background "#e6987a")))
 (spaceline-compile)
 
 ;; Ido
@@ -141,9 +174,10 @@
 (flx-ido-mode 1)
 (ido-vertical-mode 1)
 (setq ido-enable-flex-matching t)
-(set-face-attribute 'ido-vertical-first-match-face nil :background nil :foreground "#5cacee")
-(set-face-attribute 'ido-vertical-only-match-face nil :background nil :foreground "#5cacee")
-(set-face-attribute 'ido-vertical-match-face nil :foreground "#5cacee")
+(add-to-list 'configure-frame-functions '(lambda ()
+  (set-face-attribute 'ido-vertical-first-match-face nil :background nil :foreground "#5cacee")
+  (set-face-attribute 'ido-vertical-only-match-face nil :background nil :foreground "#5cacee")
+  (set-face-attribute 'ido-vertical-match-face nil :foreground "#5cacee")))
 
 ;; Tabbar
 (setq tabbar-use-images nil)
@@ -162,11 +196,32 @@
        label (max 1 (/ (window-width)
                        (length (tabbar-view
                                 (tabbar-current-tabset)))))))))
-(defun emacs-user-tabbar-groups ()
-   (list (cond ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
-               ((eq major-mode 'dired-mode) "emacs")
-               (t "user"))))
-(setq tabbar-buffer-groups-function 'emacs-user-tabbar-groups)
+; Tabbar Ruler projectile groups
+(defvar tabbar-projectile-tabbar-buffer-group-calc nil
+  "Buffer group for projectile.  Should be buffer local and speed up calculation of buffer groups.")
+(defun tabbar-projectile-tabbar-buffer-groups ()
+  "Return the list of group names BUFFER belongs to.
+    Return only one group for each buffer."
+  (if tabbar-projectile-tabbar-buffer-group-calc
+      (symbol-value 'tabbar-projectile-tabbar-buffer-group-calc)
+    (set (make-local-variable 'tabbar-projectile-tabbar-buffer-group-calc)
+         (cond
+          ((or (get-buffer-process (current-buffer)) (memq major-mode '(comint-mode compilation-mode))) '("Term"))
+          ((string-equal "*" (substring (buffer-name) 0 1)) '("Emacs"))
+          ((condition-case err
+               (projectile-project-root)
+             (error nil)) (list (projectile-project-name)))
+          ((memq major-mode '(org-mode calendar-mode diary-mode)) '("Org"))
+          ((memq major-mode '(dired-mode)) '("Dir"))
+          (t '("User"))))
+    (symbol-value 'tabbar-projectile-tabbar-buffer-group-calc)))
+(setq tabbar-buffer-groups-function 'tabbar-projectile-tabbar-buffer-groups)
+
+; (tabbar-group-by-projectile-project)
+; (defun emacs-user-tabbar-groups ()
+;    (list (cond ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
+;                ((eq major-mode 'dired-mode) "emacs")
+;                (t "user"))))
 
 ;; NeoTree
 (require 'neotree)
@@ -193,7 +248,12 @@
             (when file-name
               (neo-buffer--select-file-node file-name)))))))
 
-;; HTML / CSS / JavaScript
+;; Guide Key
+; (require 'guide-key)
+; (guide-key-mode 1)
+; (setq guide-key/guide-key-sequence '("C-l p"))
+
+;; HTML / CSS /3 JavaScript
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
@@ -269,13 +329,6 @@
 (global-unset-key (kbd "C-h"))
 (setq help-char nil)
 
-(defun switch-to-minibuffer-window ()
-  "switch to minibuffer window (if active)"
-  (interactive)
-  (when (active-minibuffer-window)
-    (select-frame-set-input-focus (window-frame (active-minibuffer-window)))
-    (select-window (active-minibuffer-window))))
-
 (require 'general)
 (setq leader-key "C-l")
 (general-define-key :prefix leader-key)
@@ -307,6 +360,7 @@
   "C-S-f" 'isearch-backward-regexp
   "C-x" 'kill-region
   "C-v" 'yank
+  "C-S-v" 'evil-visual-block
   "C-q" (lambda () (interactive) (scroll-down 1))
   "<home>" 'back-to-indentation
   "<C-tab>" 'mode-line-other-buffer
@@ -320,7 +374,9 @@
   "C-p" 'helm-mini)
 (general-define-key
   :keymaps 'ctl-x-map
+  "w" 'kill-this-buffer
   "C-b" 'neotree-projectile)
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
