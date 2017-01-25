89cb53d034ef58c6485a9e534cc4048a4f0a9cc3
Jatan Patel <j@jatan.me>
2017-01-24 17:13:09 -0700

    Update d-mode config

--- config/config-d.el
+++ config/config-d.el
@@ -1,10 +1,10 @@
-(add-hook 'd-mode-hook
-  (lambda ()
-    (c-set-offset 'substatement-open 0)
-    (c-set-offset 'case-label '+)
-    (set-local-tab-width 3)
-    (set (make-local-variable 'c-basic-offset) 3)
-    (set (make-local-variable 'c-indent-level) 3)))
+ (add-hook 'd-mode-hook
+          (lambda ()
+            (c-set-offset 'substatement-open 0)
+            (c-set-offset 'case-label '+)
+            (set-local-tab-width 2)
+            (set (make-local-variable 'c-basic-offset) 2)
+            (set (make-local-variable 'c-indent-level) 2)))
 
 (sp-local-pair 'd-mode "{" nil :post-handlers '((newline-and-enter-sexp "RET")))
 
