b6e86fb15d592c165161f4548fbd8dabb8198ca2
Jatan Patel <j@jatan.me>
2017-02-11 10:42:45 -0700

    Update emacs-lisp-mode config

--- config/config-emacs-lisp.el
+++ config/config-emacs-lisp.el
@@ -3,4 +3,11 @@
     (company-mode 1)
     (set-local-tab-width 2)))
 
+(general-define-key
+ :keymaps '(emacs-lisp-mode-map lisp-interaction-mode-map)
+ "C-c C-e" 'eval-last-sexp
+ "C-c C-r" 'eval-region
+ "C-c C-f" 'eval-defun
+ "C-c C-k" 'eval-buffer)
+
 (provide 'config-emacs-lisp)
