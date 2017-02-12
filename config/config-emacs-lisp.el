398a8777effe154121606117cbeafede97ad1ef7
Jatan Patel <j@jatan.me>
2017-02-12 12:51:52 -0700

    Update fsharp-mode config

--- config/config-emacs-lisp.el
+++ config/config-emacs-lisp.el
@@ -5,9 +5,11 @@
 
 (general-define-key
  :keymaps '(emacs-lisp-mode-map lisp-interaction-mode-map)
- "C-c C-e" 'eval-last-sexp
- "C-c C-r" 'eval-region
- "C-c C-f" 'eval-defun
- "C-c C-k" 'eval-buffer)
+ :prefix "C-c"
+ "C-e" 'eval-last-sexp
+ "C-r" 'eval-region
+ "C-c" 'eval-defun
+ "C-f" 'eval-defun
+ "C-k" 'eval-buffer)
 
 (provide 'config-emacs-lisp)
--- config/config-fsharp.el
+++ config/config-fsharp.el
@@ -2,6 +2,7 @@
 
 ;; (setq fsharp-ac-use-popup t)
 (setq fsharp-indent-offset 2)
+(setq inferior-fsharp-program "fsharpi --readline- --shadowcopyreferences+")
 
 (add-hook 'fsharp-mode-hook
   (lambda ()
@@ -10,11 +11,15 @@
 
 (sp-local-pair #'fsharp-mode "'" nil :actions nil)
 
-(add-to-list 'display-buffer-alist
-             `(,(rx bos "*inferior-fsharp*" eos)
-               (display-buffer-reuse-window
-                display-buffer-below-selected)
-               (window-height   . 0.20)))
-(evil-set-initial-state 'inferior-fsharp-mode 'emacs)
+;; (add-to-list 'display-buffer-alist
+;;              `(,(rx bos "*inferior-fsharp*" eos)
+;;                (display-buffer-reuse-window
+;;                 display-buffer-below-selected)
+;;                (window-height   . 0.20)))
+;; (evil-set-initial-state 'inferior-fsharp-mode 'emacs)
+
+(general-define-key
+ :keymaps 'fsharp-mode-map
+ "C-c C-k" 'fsharp-load-buffer-file)
 
 (provide 'config-fsharp)
