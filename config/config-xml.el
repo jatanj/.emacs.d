b85ad3f7e90b6dc8c6d6eeb2e82e2ba7e49b77e1
Jatan Patel <j@jatan.me>
2018-02-08 20:47:24 -0700

    Update xml config

--- config/config-xml.el
+++ config/config-xml.el
@@ -1,9 +1,15 @@
 (use-package nxml-mode
   :init
-  (defun xml-reformat ()
+  (defun xml-pretty-print ()
     (interactive)
     (shell-command-replace-region "xmllint --format -"))
-  (defun xml-unformat ()
+  (defun xml-escape ()
+    (interactive)
+    (web-mode-dom-xml-replace))
+  (defun xml-unescape ()
+    (interactive)
+    (web-mode-dom-entities-replace))
+  (defun xml-no-blanks ()
     (interactive)
     (shell-command-replace-region "xmllint --noblanks -"))
   (add-hook 'nxml-mode-hook
@@ -13,6 +19,8 @@
   :config
   (general-define-key
    :keymaps 'nxml-mode-map
-   "C-c C-f" 'xml-reformat))
+   "C-c C-f" 'xml-pretty-print
+   "C-c C-w" 'xml-escape
+   "C-c C-q" 'xml-unescape))
 
 (provide 'config-xml)
