5e2f3521b36d230f8dd2876111c4e52c000c3954
Jatan Patel <j@jatan.me>
2018-07-12 23:21:37 -0700

    Update lsp config

--- config/config-lsp.el
+++ config/config-lsp.el
@@ -7,21 +7,16 @@
   :init
   (add-hook 'java-mode-hook #'lsp-intellij-enable))
 
-(use-package company-lsp
-  :ensure t
-  :init
-  (add-hook 'lsp-mode-hook #'company-mode)
-  :config
-  (setq company-lsp-enable-snippet t)
-  (setq company-lsp-cache-candidates t)
-  (push 'company-lsp company-backends))
-
 (use-package lsp-ui
   :ensure t
   :init
+  (setq lsp-ui-sideline-show-symbol nil)
+  (setq lsp-ui-sideline-show-hover nil)
   (add-hook 'lsp-mode-hook
             (lambda ()
               (lsp-ui-mode 1)
+              (lsp-ui-sideline-mode 1)
+              (company-mode 1)
               (flycheck-mode 1))))
 
 (provide 'config-lsp)
