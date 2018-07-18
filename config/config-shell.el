ac5b994ac25d57d14fd1dd4fb53da2c212d650ef
Jatan Patel <j@jatan.me>
2018-07-17 22:15:38 -0700

    Add xterm-color package

--- config/config-shell.el
+++ config/config-shell.el
@@ -1,6 +1,20 @@
 (dolist (assoc '(("PKGBUILD" . shell-script-mode)))
   (add-to-list 'auto-mode-alist assoc))
 
+(use-package xterm-color
+  :ensure t
+  :init
+  (setq comint-output-filter-functions
+        (remove
+         'ansi-color-process-output
+         comint-output-filter-functions))
+  (add-hook 'shell-mode-hook
+            (lambda ()
+              (add-hook
+               'comint-preoutput-filter-functions
+               'xterm-color-filter nil t))))
+
+
 (add-hook 'sh-mode-hook
           (lambda ()
             (flycheck-mode)
