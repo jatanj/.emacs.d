3e2bb3a91624770a808cf2cac9a256ce6e559185
Jatan Patel <j@jatan.me>
2019-01-27 20:36:55 -0700

    Update flyspell config

--- config/config-flyspell.el
+++ config/config-flyspell.el
@@ -4,14 +4,15 @@
   :config
   (when (executable-find "hunspell")
     (setq ispell-program-name "hunspell")
-    (setq ispell-dictionary "en_US"))
+    (setq ispell-dictionary "en_US")
+    (setq ispell-local-dictionary-alist
+          '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8))))
   (setq flyspell-duplicate-distance 0)
   (defun flyspell-save-word-to-dictionary ()
-    (interactive)
-    (let ((word (flyspell-get-word)))
-      (when (consp word)
-        (flyspell-do-correct 'save nil (car word) (point) (cadr word) (cadr word) (point)))))
-
+      (interactive)
+      (let ((word (flyspell-get-word)))
+        (when (consp word)
+          (flyspell-do-correct 'save nil (car word) (point) (cadr word) (cadr word) (point)))))
   (general-define-key
    :keymaps 'flyspell-mode-map
    "C-c $" nil
