27d7631361e0492eac754dec0c436343737568a6
Jatan Patel <j@jatan.dev>
2021-08-15 17:57:16 -0700

    Move various functions to defuns

--- defuns.el
+++ defuns.el
@@ -139,3 +139,55 @@ With argument ARG, do this that many times."
   "Open .emacs.d in Dired."
   (interactive)
   (dired user-emacs-directory))
+
+;; Searching
+;; http://emacs.stackexchange.com/questions/10307/#answer-10432
+(defun isearch-center-cursor (&rest _)
+  (sit-for 0)
+  (if (and
+       (not (eq this-command 'isearch-other-control-char))
+       (> (length isearch-string) 0)
+       (> (length isearch-cmds) 2)
+       (let ((line (count-screen-lines (point) (window-start))))
+         (or (> line (* (/ (window-height) 4) 3))
+             (< line (* (/ (window-height) 9) 1)))))
+      (let ((recenter-position 0.3))
+        (recenter '(4)))))
+(advice-add 'isearch-update :before #'isearch-center-cursor)
+(advice-add 'evil-search :after #'isearch-center-cursor)
+
+;; Splitting
+;; https://emacs.stackexchange.com/a/40517
+(defun split-window-sensibly-prefer-horizontal (&optional window)
+  "Based on split-window-sensibly, but designed to prefer a horizontal split,
+  i.e. windows tiled side-by-side."
+  (let ((window (or window (selected-window))))
+    (or (and (window-splittable-p window t)
+             ;; Split window horizontally
+             (with-selected-window window
+               (split-window-right)))
+        (and (window-splittable-p window)
+             ;; Split window vertically
+             (with-selected-window window
+               (split-window-below)))
+        (and
+         ;; If WINDOW is the only usable window on its frame (it is
+         ;; the only one or, not being the only one, all the other
+         ;; ones are dedicated) and is not the minibuffer window, try
+         ;; to split it horizontally disregarding the value of
+         ;; `split-height-threshold'.
+         (let ((frame (window-frame window)))
+           (or
+            (eq window (frame-root-window frame))
+            (catch 'done
+              (walk-window-tree (lambda (w)
+                                  (unless (or (eq w window)
+                                              (window-dedicated-p w))
+                                    (throw 'done nil)))
+                                frame)
+              t)))
+         (not (window-minibuffer-p window))
+         (let ((split-width-threshold 0))
+           (when (window-splittable-p window t)
+             (with-selected-window window
+               (split-window-right))))))))
